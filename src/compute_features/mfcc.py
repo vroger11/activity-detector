Correct some mitakes and add normalization of data features

src/compute_features/mfcc.py
@@ -15,7 +15,7 @@ def get_mfcc_from_file(filename, windows, shift, energy=True, freq_min=1500, fre
     '''
 
     try:
-        signal, fs = librosa.load(filename)
+        signal, fs = librosa.load(filename, sr=None)
     except:
         raise
 
@@ -43,7 +43,7 @@ def get_mfcc(signal, fs, windows, shift, energy=True, freq_min=1500, freq_max=80
         energy_vec = librosa.feature.rmse(y=signal, n_fft=n_fft, hop_length=hop_length)
 
     mfcc = librosa.feature.mfcc(y=signal, sr=fs, n_mfcc=n_mfcc, fmax=freq_max, fmin=freq_min, n_fft=n_fft,
-                                hop_length=hop_length, htk=True)
+                                hop_length=hop_length, htk=True, n_mels=256)
     if energy:
         return np.vstack((energy_vec, mfcc))
     else:

src/main.py
@@ -1,14 +1,13 @@
-import src.compute_clusters.gaussian_mixtures as gausian_mixtures
-import src.compute_features.mfcc as mfcc
-import src.plotting_clusters.plot_informations as plt_clusters
 import os
 import numpy as np
 import librosa
-# from scipy.stats.mstats import zscore
+from scipy.stats import mstats
 import argparse
 import logging.config
 import ast
-
+from src.compute_clusters import gaussian_mixtures
+from src.compute_features import mfcc
+import src.plotting_clusters.plot_informations as plt_clusters
 
 def main():
     # prepare parser of arguments
@@ -52,14 +51,13 @@ def main():
 
         features = np.hstack((features, features_file)) if features != [] else features_file
 
-    # normalisation TODO see if it is useful
-    # features = zscore(features, axis=0, ddof=1) seems useless
-    features = np.transpose(
-        features)  # model require n*d; with n the number of observations and d the dimension of an observation
+    features = mstats.zscore(features, axis=1, ddof=1)
+    # model require n*d; with n the number of observations and d the dimension of an observation
+    features = np.transpose(features)
 
     # learn model
     logger.info("Learning model")
-    model = gausian_mixtures.Model(n_components=30, n_iter=100, alpha=.5, verbose=0, covariance_type="diag")
+    model = gaussian_mixtures.Model(n_components=30, n_iter=100, alpha=.5, verbose=0, covariance_type="diag")
     model.learn_model(features)
     logger.info("Done. Converged: " + str(model.dpgmm_model.converged_))
 
@@ -73,7 +71,7 @@ def main():
     for file in files:
         path_to_file = os.path.normpath(args.folder_audio + "/" + file)
         try:
-            signal_file, fs = librosa.load(path_to_file)
+            signal_file, fs = librosa.load(path_to_file, sr=None)
         except:
             logger.warning("There is a problem while reading: " + path_to_file)
             continue

src/plotting_clusters/plot_informations.py
@@ -12,7 +12,7 @@ def show_audio(filename):
     """
 
     # read the audio file
-    signal, sample_rate = librosa.load(filename)
+    signal, _ = librosa.load(filename, sr=None)
 
     _, axarr = plt.subplots(2, sharex=True)
     # plot the waveform
@@ -102,7 +102,7 @@ def vector_of_cluster_to_matrix(vector, number_max=None):
 
 # TODO replace it by a test
 if __name__ == '__main__':
-    signal, sample_rate = librosa.load(sys.argv[1])
+    signal, sample_rate = librosa.load(sys.argv[1], sr=None)
     vector = np.zeros(shape=(len(signal)))
 
     # to test the function, create fake clusters