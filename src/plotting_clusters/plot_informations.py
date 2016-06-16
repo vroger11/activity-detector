c8083fa0b37545383535ca6015f4efef1a68d09b
Vincent ROGER <roger.vins.11@gmail.com>
2016-06-16 10:43:50 +0200

    Add a saving function of all audio and change vector_of_cluster_to_matrix prototype. Will break the main.py file

--- src/plotting_clusters/plot_informations.py
+++ src/plotting_clusters/plot_informations.py
@@ -1,7 +1,6 @@
 import numpy as np
 import librosa
 import matplotlib.pyplot as plt
-import sys
 
 
 def show_audio(filename):
@@ -12,23 +11,22 @@ def show_audio(filename):
     """
 
     # read the audio file
-    signal, _ = librosa.load(filename, sr=None)
+    # plot the waveform
+    signal, sample_rate = librosa.load(filename, sr=None)
 
     _, axarr = plt.subplots(2, sharex=True)
-    # plot the waveform
     time = np.linspace(0, len(signal) / sample_rate, num=len(signal))
     axarr[0].plot(time, signal)
     axarr[0].set_title('Waveform')
     axarr[0].set_ylabel("Amplitude")
 
     # plot the spectgram
-    _, _, t, im = axarr[1].specgram(signal, NFFT=1024, Fs=sample_rate,
-                                               noverlap=512, window=np.hamming(1024))
+    axarr[1].specgram(signal, NFFT=1024, Fs=sample_rate,
+                      noverlap=512, window=np.hamming(1024))
     axarr[1].set_title('Spectrogram')
     axarr[1].set_ylabel("Frequency in Hz")
     axarr[1].set_xlabel("Time in seconds")
     plt.show()
-
 
 def show_audio_with_cluster(signal, sample_rate, cluster, show_signal=True, show_spectrogram=True):
     '''
@@ -40,11 +38,39 @@ def show_audio_with_cluster(signal, sample_rate, cluster, show_signal=True, show
     :param cluster: matrix of s*n, were s is the clusters an n the of samples of the signal
     :param show_signal: show the subplot of the signal
     :param show_spectrogram: show the subplot of the spectrogram
-    :return: the cluster corresponding to the signal
     '''
+    figure = _compute_figure(signal=signal,
+                             sample_rate=sample_rate,
+                             cluster=cluster,
+                             show_signal=show_signal,
+                             show_spectrogram=show_spectrogram)
+    figure.show()
+    plt.show()
+
+def save_audio_with_cluster(filename_out, signal, sample_rate, cluster, show_signal=True, show_spectrogram=True):
+    '''
+    compute the figure to show/save
+
+    :param signal: one channel signal
+    :param sample_rate: sampling frequency (in Hz)
+    :param cluster: matrix of s*n, were s is the clusters an n the of samples of the signal
+    :param show_signal: show the subplot of the signal
+    :param show_spectrogram: show the subplot of the spectrogram
+    '''
+    figure = _compute_figure(signal=signal,
+                             sample_rate=sample_rate,
+                             cluster=cluster,
+                             show_signal=show_signal,
+                             show_spectrogram=show_spectrogram)
+
+    figure.savefig(filename_out, dpi=200, bbox_inches='tight', papertype='ledger')
+    plt.close()
+
+
+def _compute_figure(signal, sample_rate, cluster, show_signal=True, show_spectrogram=True):
 
     total_subplots = 1 + show_signal + show_spectrogram
-    _, axarr = plt.subplots(total_subplots, sharex=True)
+    figure, axarr = plt.subplots(total_subplots, sharex=True)
 
     id_subplot = 0
     if show_signal:
@@ -57,9 +83,11 @@ def show_audio_with_cluster(signal, sample_rate, cluster, show_signal=True, show
 
     if show_spectrogram:
         # plot the spectgram
-        _, _, _, im = axarr[id_subplot].specgram(
-            signal, NFFT=1024, Fs=sample_rate, noverlap=512,
-            window=np.hamming(1024))
+        axarr[id_subplot].specgram(signal,
+                                   NFFT=1024,
+                                   Fs=sample_rate,
+                                   noverlap=512,
+                                   window=np.hamming(1024))
         axarr[id_subplot].set_title('Spectrogram')
         axarr[id_subplot].set_ylabel("Frequency in Hz")
         id_subplot += 1
@@ -68,47 +96,36 @@ def show_audio_with_cluster(signal, sample_rate, cluster, show_signal=True, show
     m = np.matrix(cluster)
     dim_x, _ = m.shape
     cmap = plt.get_cmap("nipy_spectral")
-    axarr[id_subplot].matshow(m, aspect='auto', origin='lower', extent=[0, len(signal) / sample_rate, 0, dim_x], cmap=cmap)
+    axarr[id_subplot].matshow(m,
+                              aspect='auto',
+                              origin='lower',
+                              extent=[0, len(signal) / sample_rate, 0, dim_x],
+                              cmap=cmap)
     axarr[id_subplot].set_ylabel("Cluster")
     axarr[id_subplot].set_xlabel("Time in seconds")
     axarr[id_subplot].xaxis.set_ticks_position('bottom')
     # TODO find how to add grid
 
-    plt.show()
+    return figure
 
-
-def vector_of_cluster_to_matrix(vector, number_max=None):
+def vector_of_cluster_to_matrix(vector, values_possible=None):
     '''
     :param vector: should contains values >= 0
-    :param number_max: the number of clusters. If it is equal to None the
-                       function will find the adequat value from vector
+    :param values_possible: a list of possible values in vector
     :return: matrix that can be used in the show_signal_with_cluster function
     '''
 
-    values_possible = list(np.unique(vector))
-    if number_max == None:
+    if values_possible is None:
+        values_possible = list(np.unique(vector))
         color_values = 1 / (len(values_possible) + 1)
         clusters = np.ones(shape=(len(values_possible), len(vector)))
     else:
+        number_max = len(values_possible)
         color_values = 1 / (number_max + 1)
         clusters = np.ones(shape=(number_max, len(vector)))
 
     for i in range(len(vector)):
-        index = values_possible.index(vector[i]) if number_max == None else vector[i]
+        index = values_possible.index(vector[i])
         clusters[index, i] = color_values * index
 
     return clusters
-
-
-# TODO replace it by a test
-if __name__ == '__main__':
-    signal, sample_rate = librosa.load(sys.argv[1], sr=None)
-    vector = np.zeros(shape=(len(signal)))
-
-    # to test the function, create fake clusters
-    vector[0:len(signal) / 2] = 1
-    vector[len(signal) / 2:len(signal) * 3 / 4] = 2
-
-    clusters = vector_of_cluster_to_matrix(vector)
-
-    show_audio_with_cluster(signal, sample_rate, clusters)
