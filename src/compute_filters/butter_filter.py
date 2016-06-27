c3a74e0d166c76f17d59d5c8dd0a62ebc761dba3
Vincent ROGER <vroger@localhost.localdomain>
2016-06-27 10:45:23 +0200

    Add butter low,high and band filter

--- src/compute_filters/butter_filter.py
+++ src/compute_filters/butter_filter.py
@@ -1,16 +1,21 @@
+from scipy.signal import butter, lfilter
 
+def butter_bandpass_filter(data, lowcut, highcut, sample_rate, order=5):
+    nyq = 0.5 * sample_rate
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='bandpass')
+    y = lfilter(b, a, data)
+    return y
 
+def butter_low_pass_filter(data, sample_rate, low_cut):
+    low = lowcut / (0.5 * sample_rate)
+    b, a = butter(order, low, btype='lowpass')
+    y = lfilter(b, a, data)
+    return y
 
-
-
-def low_high_pass_filter(data, fs, freq_cut, high=True):
-    """
-
-    :param data:
-    :param fs:
-    :param freq_cut:
-    :param high:
-    :return:
-    """
-
-
+def butter_high_pass_filter(data, sample_rate, high_cut):
+    high = highcut / (0.5 * sample_rate)
+    b, a = butter(order, high, btype='highpass')
+    y = lfilter(b, a, data)
+    return y
