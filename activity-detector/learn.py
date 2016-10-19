6bcf14d5f3bc5c4454c3b12bba6b21311a5fefb9
vroger <roger.vins.11@gmail.com>
2016-10-19 16:02:48 +0200

    Modify how the ml parameter is used

--- activity-detector/learn.py
+++ activity-detector/learn.py
@@ -7,6 +7,7 @@ import argparse
 import logging.config
 import ast
 import pickle
+import random
 import numpy as np
 from scipy.stats import mstats
 #from sklearn import metrics
@@ -26,22 +27,27 @@ def learn_model(folder_audio, feature_extractor, max_learn):
     """
         :param folder_audio: folder where the audios are
         :param feature_extractor: object use to extract features
-        :param max_learn: number of file used
+        :param max_learn: between 0 and 1, help to define the number of files
+                          use in every folder. 1 means every file is used
 
         :return: model learned
     """
     # get features
     LOGGER.info("Getting features")
     features = []
-    file_taken = 0
+    file_taken = []
     for root, _, files in os.walk(folder_audio):
         LOGGER.info("Getting features from: " + root)
 
-        for file in files:
-            # stop taking files when we reach the limit
-            if (not max_learn is None) and file_taken == max_learn:
-                break
+        if (not max_learn is None):
+            number_of_file = int(len(files)*max_learn)
+            random.shuffle(files)
+            files_to_learn = files[0:number_of_file]
+        else:
+            files_to_learn = files
 
+        file_taken.append(files_to_learn)
+        for file in files_to_learn:
             path_to_file = os.path.join(root, file)
 
             try:
@@ -51,7 +57,6 @@ def learn_model(folder_audio, feature_extractor, max_learn):
                 continue
 
             features = np.hstack((features, features_file)) if features != [] else features_file
-            file_taken += 1
 
     features = mstats.zscore(features, axis=1, ddof=1)
     # model require n*d; with n the number of observations and d the dimension of an observation
@@ -67,7 +72,7 @@ def learn_model(folder_audio, feature_extractor, max_learn):
     _, values_possible = model.learn_model(features)
     LOGGER.info("Done. Converged: " + str(model.dpgmm_model.converged_))
 
-    return [model, values_possible]
+    return [model, values_possible, file_taken]
 
 def main(args):
     if not os.path.exists(os.path.join(args.folder_out, "model")):
@@ -79,13 +84,17 @@ def main(args):
                                     n_mfcc=26,
                                     energy=True)
 
-    model, values_possible = learn_model(args.folder_audio,
-                                         feature_extractor,
-                                         args.max_learn)
+    model, values_possible, file_taken = learn_model(args.folder_audio,
+                                                     feature_extractor,
+                                                     args.max_learn)
 
     save_obj(model, os.path.join(args.folder_out, 'model/model'))
     save_obj(values_possible, os.path.join(args.folder_out, 'model/values_possible'))
     save_obj(feature_extractor, os.path.join(args.folder_out, 'model/feature_extractor'))
+    file_out_taken = open(os.path.join(args.folder_out, 'model/file_taken.txt'), 'w')
+    for item in file_taken:
+        file_out_taken.write("%s\n" % item)
+
     # TODO compute indices performances
 #    plot_internal_indices.plot_silhouette(silhouette_score)
 
@@ -102,8 +111,9 @@ if __name__ == '__main__':
                         help='minimum frequency')
     PARSER.add_argument('freq_max', metavar='max_frequency', type=int,
                         help='maximum frequency')
-    PARSER.add_argument('-ml', '--max_learn', type=int,
-                        help='maximum number of file to take for learning the model.' +
+    PARSER.add_argument('-ml', '--max_learn', type=float,
+                        help='between 0 and 1, help to define the number of files' +
+                        ' use in every folder. 1 means every file is used.\n' +
                         ' By default all of them',
                         default=None)
     PARSER.add_argument('-v', '--verbose', action='store_true', help='Show every log')
